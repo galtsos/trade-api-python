51ce309b976f4899c984307d906a7c292e0d60f6
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2019-12-19 22:30:51 +0400

    Extract technical code to a new package

--- examples/example.py
+++ examples/example.py
@@ -1,32 +1,26 @@
 import asyncio
 import datetime
-import signal
-from typing import Sequence
+from typing import Dict, Sequence
 
+from galts_trade_api.asyncio_helper import AsyncProgramEnv, run_program_forever
 from galts_trade_api.exchange import DepthConsumeKey
 from galts_trade_api.terminal import Terminal
 from galts_trade_api.transport.real import RealTransportFactory
 
 
-async def start_trade_system():
+async def start_trade_system(program_env: AsyncProgramEnv):
     username = 'vasya'
     password = 'pupkin123'
     symbol_tag = 'BTCUSDT'
 
-    def handle_exception(local_loop: asyncio.AbstractEventLoop, context):
+    def exception_handler(local_loop: asyncio.AbstractEventLoop, context: Dict) -> None:
         nonlocal terminal
 
         if 'terminal' in locals():
             terminal.shutdown_transport()
             del terminal
 
-        print('Caught exception (main process):')
-        local_loop.default_exception_handler(context)
-
-        local_loop.create_task(shutdown(local_loop))
-
-    loop = asyncio.get_running_loop()
-    loop.set_exception_handler(handle_exception)
+    program_env.exception_handler_patch = exception_handler
 
     transport = RealTransportFactory()
     transport.configure_endpoints(
@@ -75,43 +69,5 @@ async def on_price(
         f'len(bids)={len(bids)} len(asks)={len(asks)}')
 
 
-async def signal_handler(signal, loop: asyncio.AbstractEventLoop):
-    print(f'Received exit signal {signal.name}...')
-    await shutdown(loop)
-
-
-# Inspired by https://www.roguelynn.com/words/asyncio-exception-handling/
-async def shutdown(loop: asyncio.AbstractEventLoop):
-    """Cleanup tasks tied to the program's shutdown."""
-    print('Shutting down...')
-
-    other_tasks = [t for t in asyncio.all_tasks(loop) if t is not asyncio.current_task(loop)]
-
-    [task.cancel() for task in other_tasks]
-
-    print(f'Cancelling {len(other_tasks)} outstanding tasks')
-    await asyncio.gather(*other_tasks, return_exceptions=True)
-
-    loop.stop()
-
-
-def main():
-    loop = asyncio.new_event_loop()
-    loop.set_debug(True)
-
-    signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
-    for s in signals:
-        loop.add_signal_handler(s, lambda s=s: loop.create_task(signal_handler(s, loop)))
-
-    try:
-        loop.create_task(start_trade_system())
-        loop.run_forever()
-    finally:
-        # Only with run_forever()
-        loop.run_until_complete(loop.shutdown_asyncgens())
-        loop.close()
-        print('Successfully shutdown the program.')
-
-
 if __name__ == '__main__':
-    main()
+    run_program_forever(start_trade_system, loop_debug=True)
---
+++ src/galts_trade_api/asyncio_helper.py
@@ -0,0 +1,85 @@
+import asyncio
+import os
+import signal
+from typing import Any, Awaitable, Callable, Dict, Optional, Sequence
+
+LoopExceptionHandlerCallable = Callable[[asyncio.AbstractEventLoop, Dict[str, Any]], Any]
+
+
+async def signal_handler(signal, loop: asyncio.AbstractEventLoop):
+    print(f'Received exit signal {signal.name}...')
+    await shutdown(loop)
+
+
+# Inspired by https://www.roguelynn.com/words/asyncio-exception-handling/
+async def shutdown(loop: asyncio.AbstractEventLoop):
+    """Cleanup tasks tied to the program's shutdown."""
+    print('Shutting down...')
+
+    other_tasks = [t for t in asyncio.all_tasks(loop) if t is not asyncio.current_task(loop)]
+
+    [task.cancel() for task in other_tasks]
+
+    print(f'Cancelling {len(other_tasks)} outstanding tasks')
+    await asyncio.gather(*other_tasks, return_exceptions=True)
+
+    loop.stop()
+
+
+def run_program_forever(
+    target: Callable[..., Awaitable],
+    loop: Optional[asyncio.AbstractEventLoop] = None,
+    loop_debug: Optional[bool] = None,
+    handle_signals: Optional[Sequence[signal.Signals]] = None
+) -> None:
+    if not loop:
+        loop = asyncio.new_event_loop()
+
+    if loop_debug is not None:
+        loop.set_debug(loop_debug)
+
+    if handle_signals is None:
+        handle_signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
+
+    for sig in handle_signals:
+        loop.add_signal_handler(sig, lambda s=sig: loop.create_task(signal_handler(s, loop)))
+
+    try:
+        env = AsyncProgramEnv()
+        loop.set_exception_handler(env.exception_handler)
+        loop.create_task(target(env))
+        loop.run_forever()
+    finally:
+        loop.run_until_complete(loop.shutdown_asyncgens())
+        loop.close()
+        print('Successfully shutdown the program.')
+
+
+class AsyncProgramEnv:
+    """
+    Main purpose of the class is give an ability to "patch" an event loop exception handler.
+    A user can do additional shutdown tasks in the patch.
+    """
+
+    def __init__(self):
+        self._exception_handler_patch: Optional[LoopExceptionHandlerCallable] = None
+
+    @property
+    def exception_handler_patch(self):
+        return self._exception_handler_patch
+
+    @exception_handler_patch.setter
+    def exception_handler_patch(self, value: Optional[LoopExceptionHandlerCallable] = None):
+        if value is not None and not callable(value):
+            raise TypeError('Value should be a callable or None')
+
+        self._exception_handler_patch = value
+
+    def exception_handler(self, loop: asyncio.AbstractEventLoop, context: Dict[str, Any]):
+        if self.exception_handler_patch:
+            self.exception_handler_patch(loop, context)
+
+        print(f'Caught exception (process #{os.getpid()}):')
+        loop.default_exception_handler(context)
+
+        loop.create_task(shutdown(loop))
