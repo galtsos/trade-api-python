25f5dbe5418ff870b59138dcfb5dee5380685a1a
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2020-01-07 22:06:18 +0400

    Add unit-test about singleton logic

--- tests/transport/real_test.py
+++ tests/transport/real_test.py
@@ -7,14 +7,22 @@ from ..utils import AsyncMock
 
 
 class TestRabbitConnection:
+    def test_constructor_is_singleton(self):
+        conn_a = RabbitConnection('test1.local')
+        conn_b = RabbitConnection('test1.local')
+        conn_c = RabbitConnection('test2.local')
+
+        assert conn_a is conn_b
+        assert conn_a is not conn_c
+
     def test_constructor_dont_init_connection_object(self):
-        conn = RabbitConnection('test1.local')
+        conn = RabbitConnection('test3.local')
 
         assert conn.connection is None
 
     @pytest.mark.asyncio
     async def test_constructor_trim_dsn(self):
-        dsn = ' \t test2.local  '
+        dsn = ' \t test4.local  '
         expected_dsn = dsn.strip()
         assert dsn != expected_dsn
 
@@ -27,7 +35,7 @@ class TestRabbitConnection:
 
     @pytest.mark.asyncio
     async def test_create_channel_reuse_one_connection(self):
-        conn = RabbitConnection('test3.local')
+        conn = RabbitConnection('test5.local')
 
         with patch('aio_pika.connect_robust', new_callable=AsyncMock):
             await conn.create_channel()
@@ -38,7 +46,7 @@ class TestRabbitConnection:
 
     @pytest.mark.asyncio
     async def test_create_channel_use_delivered_dsn(self):
-        dsn = 'test4.local'
+        dsn = 'test6.local'
         conn = RabbitConnection(dsn)
         prefetch_count = 50
 
