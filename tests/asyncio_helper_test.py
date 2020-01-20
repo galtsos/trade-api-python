e19b634d5d6beb534b979a1ec03ecf14669d31e2
Sergey Nevmerzhitsky <sergey.nevmerzhitsky@gmail.com>
2020-01-20 19:30:16 +0400

    Add unit-tests of shutdown()

--- tests/asyncio_helper_test.py
+++ tests/asyncio_helper_test.py
@@ -5,7 +5,8 @@ from unittest.mock import ANY, Mock
 
 import pytest
 
-from galts_trade_api.asyncio_helper import AsyncProgramEnv, run_program_forever, signal_handler
+from galts_trade_api.asyncio_helper import AsyncProgramEnv, run_program_forever, shutdown, \
+    signal_handler
 from .utils import AsyncMock
 
 
@@ -23,6 +24,60 @@ def test_signal_handler_log_and_shutdown(mocker):
         process_id=ANY,
         signal='SIGTERM'
     )
+
+
+class TestShutdown:
+    @pytest.mark.asyncio
+    async def test_log_and_stop_loop(self, mocker):
+        logger_mock = mocker.patch('galts_trade_api.asyncio_helper.logger')
+        loop = Mock(spec_set=asyncio.AbstractEventLoop)
+
+        await shutdown(loop)
+
+        loop.stop.assert_called_once()
+        logger_mock.info.assert_called_once_with('Shutting down', process_id=ANY)
+        logger_mock.debug.assert_called_once_with(
+            'Cancelling outstanding tasks',
+            process_id=ANY,
+            amount=0
+        )
+
+    @pytest.mark.asyncio
+    async def test_cancel_tasks(self, mocker):
+        logger_mock = mocker.patch('galts_trade_api.asyncio_helper.logger')
+        loop = Mock(spec_set=asyncio.AbstractEventLoop)
+        current_task_mock = mocker.patch('asyncio.current_task', autospec=True)
+        task1 = Mock(spec_set=asyncio.Task)
+        task1().cancelled.return_value = True
+        task2 = Mock(spec_set=asyncio.Task)
+        task2().cancelled.return_value = False
+        task2().exception.return_value = None
+        task3 = Mock(spec_set=asyncio.Task)
+        task3().cancelled.return_value = False
+        task3_exception = Exception('exception in task')
+        task3().exception.return_value = task3_exception
+        all_tasks_mock = mocker.patch('asyncio.all_tasks', autospec=True)
+        all_tasks_mock.return_value = [task1(), task2(), task3()]
+        gather_mock = mocker.patch('asyncio.gather', new_callable=AsyncMock)
+
+        await shutdown(loop)
+
+        all_tasks_mock.assert_called_once_with(loop)
+        current_task_mock.assert_called_with(loop)
+        task1().cancel.assert_called_with()
+        task2().cancel.assert_called_with()
+        task3().cancel.assert_called_with()
+        gather_mock.assert_called_with(task1(), task2(), task3(), return_exceptions=True, loop=loop)
+        loop.default_exception_handler.assert_called_once_with({
+            'message': 'Unhandled exception during shutdown',
+            'exception': task3_exception,
+            'task': task3(),
+        })
+        logger_mock.debug.assert_called_once_with(
+            'Cancelling outstanding tasks',
+            process_id=ANY,
+            amount=3
+        )
 
 
 def fixture_setup_loop_by_arguments():
